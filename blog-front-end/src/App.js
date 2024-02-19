6cb9dd99bf35041a85311b8254f957a0f4cdd400
kevinlam0 <kevinclam02@gmail.com>
2024-02-19 15:00:58 -0500

    reused the form

--- blog-front-end/src/App.js
+++ blog-front-end/src/App.js
@@ -3,7 +3,6 @@ import { Header } from "./components/Header";
 import { OpeningPictures } from "./components/OpeningPictures";
 import { Comments } from './components/Comments';
 import { CommentForm } from './components/AddComments'
-import { CommentSection } from 'replyke';
 function App() {
 
   return (
@@ -11,10 +10,7 @@ function App() {
       <Header/>
       <OpeningPictures/>
       <Comments/>
-      <CommentSection
-        articleId="UNIQUE_ARTICLE_ID"
-        styleId="STYLE_ID"
-      />
+      <CommentForm/>
     </div>
   );
 }

8dc167fec46b3db947c630c0fddd5c17ab11b249
kevinlam0 <kevinclam02@gmail.com>
2024-02-19 11:01:11 -0500

    added function for adding comments

--- BlogPostSpring/src/main/java/com/kevinlam/BlogPost/BlogPostController.java
+++ BlogPostSpring/src/main/java/com/kevinlam/BlogPost/BlogPostController.java
@@ -1,11 +1,9 @@
 package com.kevinlam.BlogPost;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -19,5 +17,13 @@ public class BlogPostController {
     @GetMapping("/comments")
     public List<Comment> getAllComments() {
         return service.getAllComments();
+    }
+
+    @PostMapping
+    public void addComment(@RequestBody Comment c) {
+        long millis=System.currentTimeMillis();
+        Date current = new Date(millis);
+        c.setDate(current);
+        service.addComment(c);
     }
 }

2fea7eac7e22f9316af828d887818838065576e0
kevinlam0 <kevinclam02@gmail.com>
2024-02-19 11:01:57 -0500

    began with commetn form

--- blog-front-end/src/App.js
+++ blog-front-end/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {useEffect, useState} from "react";
 import { Header } from "./components/Header";
 import { OpeningPictures } from "./components/OpeningPictures";
 import { Comments } from './components/Comments';
--- blog-front-end/src/components/AddComments.jsx
+++ blog-front-end/src/components/AddComments.jsx
@@ -1,26 +1,34 @@
 import React, { useState, useContext } from 'react';
 export const CommentForm = () => {
     const [comment, setComment] = useState( '' )
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     fetch('http://localhost:8080/blogpost/comments'), {
-    //         method: 'POST',
-    //         headers: 
-    //     }
-    // }
 
-    const handleChange = (e) => {
-        const { comment, value } = e.target;
-        setComment(prevComment => ({ ...prevComment, }))
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:8080/blogpost/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(comment),
+        })
+        .then(response => response.json())
+        .then(data => console.log("user created"))
+        .catch(error => console.error("Error creating comment: ", error))
     }
+
+    // const handleChange = (e) => {
+    //     const { comment, value } = e.target;
+    //     setComment(prevComment => ({ ...prevComment, }))
+    // }
     return (
-        <form >
+        <form onSubmit={handleSubmit}>
             <input
                 type='text'
                 name='name'
                 value={comment}
                 onChange={(e) => setComment(e.target.value)}
                 placeholder='Enter Comment...' />
+            <button type="submit">Submit</button>
         </form>
     )
 }

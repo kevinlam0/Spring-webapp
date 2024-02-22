7fcb769859dbe2cd37adf413926884974de849de
kevinlam0 <kevinclam02@gmail.com>
2024-02-22 15:59:16 -0500

    have a button to toggle showing comment or not

--- blog-front-end/src/pages/BlogPost.jsx
+++ blog-front-end/src/pages/BlogPost.jsx
@@ -3,15 +3,29 @@ import { OpeningPictures } from "../components/OpeningPictures";
 import { CommentSection } from '../components/CommentSection';
 import { useNavigate } from "react-router-dom"
 import NavBar from "../NavBar";
+import { useState } from 'react';
 
 export const BlogPost = () => {
+  const [viewComment, setViewComment] = useState(false);
     const navigate = useNavigate();
+
+    const toggleViewComment = () => {
+      setViewComment(!viewComment);
+    }
     return (
       <div>
         <NavBar/>
         <Header/>
         <OpeningPictures/>
-        <CommentSection/>
+        {
+          viewComment ? 
+            <div>
+              <button onClick={toggleViewComment}>Close comments</button>
+              <CommentSection/>
+            </div>
+          :
+            <button onClick={toggleViewComment}>View Comments</button>
+        }
       </div>
     );
 }
